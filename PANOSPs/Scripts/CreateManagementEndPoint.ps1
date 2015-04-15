b6dc69c52796ff8198da37a8d3e82408e0bf015d
alextc <alextc@microsoft.com>
2015-04-15 16:42:11 -0700

    Updated CreateManagementEndPoint script: 1. Added a Get-BlockedTraffic script instead of the corresponding CmdLet. 2. Combined New-PSSessionConfigurationFile and Register-PSSessionConfiguration in a single script

--- PANOSPs/Scripts/CreateManagementEndPoint.ps1
+++ PANOSPs/Scripts/CreateManagementEndPoint.ps1
@@ -1,18 +1,31 @@
-﻿Unregister-pssessionconfiguration -name FirewallManagement -force
+﻿$getBlockedTraffic = {
+    param ( [string]$SourceIp, [DateTime]$RangeStart = (Get-Date).AddHours(-4), [DateTime]$RangeEnd = (Get-Date) )
+    $accessToken = convertto-securestring "" -asplaintext -force
+    $query =  [string]::Format("( action eq deny) and ( addr.src in {0} ) and ( receive_time leq '{1:yyyy/MM/dd HH:mm:ss}' ) and (receive_time geq '{2:yyyy/MM/dd HH:mm:ss}')", $SourceIp, $RangeEnd, $RangeStart)
+    $connectionPropertyFW1 = New-PANOSConnectionProperties -HostName 'firewall1.it.msft.net' -Vsys 'vsys3' -AccessToken $accessToken
+    $connectionPropertyFW2 = New-PANOSConnectionProperties -HostName 'firewall2.it.msft.net' -Vsys 'vsys3' -AccessToken $accessToken
+    Get-PANOSTrafficLog -Query $query  -ConnectionProperties $connectionPropertyFW1,$connectionPropertyFW2 | Format-Table
+}
 
 New-PSSessionConfigurationFile -Path c:\PSScripts\panos.pssc `
+                                -ModulesToImport PANOS, Microsoft.PowerShell.Utility `
                                -Description 'PANOS Delegation EndPoint' `
                                -ExecutionPolicy Restricted `
-                               -ModulesToImport PANOS, Microsoft.PowerShell.Utility `
-                               -VisibleCmdlets 'Get-PANOSBlockedTraffic', 'Format-Table' `
-                               -SessionType RestrictedRemoteServer
+                               -VisibleCmdlets 'Format-Table', 'Get-Help', 'Select-Object', 'Get-Date' `
+                               -SessionType RestrictedRemoteServer `
+                               -LanguageMode FullLanguage `
+                               -FunctionDefinitions @{Name="Get-BlockedTraffic";ScriptBlock=$getBlockedTraffic; Options="AllScope"}
 
+Unregister-pssessionconfiguration -name FirewallManagement -force
 Test-PSSessionConfigurationFile -Path c:\PSScripts\panos.pssc
 
-$secpasswd = ConvertTo-SecureString "PASSWORDHERE" -AsPlainText -Force
+$secpasswd = ConvertTo-SecureString "" -AsPlainText -Force
 $sessionCreds = New-Object System.Management.Automation.PSCredential ("cxeredxjea02\panosjea", $secpasswd)
-Register-PSSessionConfiguration -Path c:\PSScripts\panos.pssc `
+
+Register-PSSessionConfiguration -Path 'c:\PSScripts\panos.pssc' `
                                 -Name FirewallManagement `
                                 -ShowSecurityDescriptorUI `
+                                -RunAsCredential $sessionCreds `
                                 -AccessMode Remote `
-                                -RunAsCredential $sessionCreds
+                                -Force
+                                
---
+++ PANOSPs/Scripts/Get-BlockedTrafficLog.ps1
@@ -0,0 +1,12 @@
+﻿#( action eq deny) and ( addr.src in 10.127.63.254 ) and ( receive_time leq '2015/03/16 12:46:23' ) and (receive_time geq '2015/03/16 00:45:00')
+function Get-BlockedTraffic
+{
+    param ( [string]$SourceIp, [DateTime]$RangeStart = (Get-Date).AddHours(-4), [DateTime]$RangeEnd = (Get-Date) )
+    $query =  [string]::Format("( action eq deny) and ( addr.src in {0} ) and ( receive_time leq '{1:yyyy/MM/dd HH:mm:ss}' ) and (receive_time geq '{2:yyyy/MM/dd HH:mm:ss}')", $SourceIp, $RangeEnd, $RangeStart)
+    $connectionPropertyFW1 = New-PANOSConnectionProperties -HostName 'firewall1.it.msft.net' -Vsys 'vsys3' -AccessToken (Import-CliXml c:\PSScripts\panosAccessToken.txt)
+    $connectionPropertyFW2 = New-PANOSConnectionProperties -HostName 'firewall2.it.msft.net' -Vsys 'vsys3' -AccessToken (Import-CliXml c:\PSScripts\panosAccessToken.txt)
+    Get-PANOSTrafficLog -Query $query  -ConnectionProperties $connectionPropertyFW1,$connectionPropertyFW2 | Select-Object ReceiveTime, Destination, App | ft
+}
+
+Get-BlockedTraffic '10.193.160.67' (Get-Date).AddDays(-1) (Get-Date)
+#Get-BlockedTraffic '10.193.160.67'
