24a9b415947db8296d933b115032ab99f0ff7802
alextc <alextc@microsoft.com>
2015-04-24 11:18:04 -0700

    Added Help for Remove-PANOSAddress

---
+++ PANOSPs/Scripts/Address/RemoveAddressExamples.ps1
@@ -0,0 +1,4 @@
+﻿$connection = New-PANOSConnection -HostName 'palab.redmond.corp.microsoft.com' -Vsys 'vsys1' -AccessToken (Import-CliXml c:\PSScripts\panosAccessToken)
+Remove-PANOSAddress -Connection $connection -Name API-TEST-10897
+Get-PANOSAddress -Connection $connection -Name API-TEST-11745 | Remove-PANOSAddress -Connection $connection
+Get-PANOSAddress -Connection $connection -FromCandidateConfig | where { $_.Address -eq '10.160.49.218' } | Remove-PANOSAddress -Connection $connection
