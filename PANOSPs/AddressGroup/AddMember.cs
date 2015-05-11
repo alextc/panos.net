2bb1f1629dc663e51b13ec451791ffa3e5fa06a8
alextc <alextc@microsoft.com>
2015-05-11 16:06:36 -0700

    Added Add-PANOSAddressGroupMember Cmdlet

---
+++ PANOSPs/AddressGroup/AddMember.cs
@@ -0,0 +1,37 @@
+﻿namespace PANOS
+{
+    using System.Management.Automation;
+
+    [Cmdlet(VerbsCommon.Add, "PANOSAddressGroupMember")]
+    public class AddMember : RequiresConnection
+    {
+        private IMembershipRepository membershipRepository;
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipeline = true)]
+        [ValidatePattern("^[A-Za-z0-9-_.]+$")]
+        public string GroupName { get; set; }
+
+        [Parameter(
+           Mandatory = true,
+           ValueFromPipeline = true)]
+        [ValidatePattern("^[A-Za-z0-9-_.]+$")]
+        public string MemberName { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+            membershipRepository = new MembershipRepository(
+               new ConfigMembershipCommandFactory(
+                   new ApiUriFactory(Connection.Host),
+                   new ConfigMembershipPostKeyValuePairFactory(Connection.AccessToken, Connection.Vsys))
+               );
+        }
+
+        protected override void ProcessRecord()
+        {
+            membershipRepository.AddMember(GroupName, Schema.AddressGroupSchemaName, MemberName);
+        }
+    }
+}
---
+++ PANOSPsTests/PsAddMemberTests.cs
@@ -0,0 +1,51 @@
+﻿namespace PANOSPsTest
+{
+    using System.Linq;
+    using NUnit.Framework;
+    using PANOS;
+
+    [TestFixture(typeof(AddressGroupObject), typeof(AddressObject), typeof(GetSingleAddressGroupApiResponse),  "PANOSAddressGroupMember", Schema.AddressGroupSchemaName)]
+    public class PsAddMemberTests<TGroup, TMember, TGroupDeserializer> : BasePsTest
+        where TGroup : GroupFirewallObject
+        where TMember : FirewallObject
+        where TGroupDeserializer : ApiResponseForGetSingle
+    {
+        private readonly string noun;
+        private readonly PsTestRunner<TGroup> psTestRunner;
+        private readonly ISearchableRepository<TGroup> searchableRepository;
+        
+        public PsAddMemberTests(string noun, string groupSchemaName)
+        {
+            this.noun = noun;
+            psTestRunner = new PsTestRunner<TGroup>();
+            searchableRepository = new SearchableRepository<TGroup>(ConfigCommandFactory, groupSchemaName);
+        }
+
+        [Test]
+        public void ShouldAddSingleMemberPassedAsParameter()
+        {
+            // Setup
+            var sut = RandomObjectFactory.GenerateRandomObject<TGroup>();
+            AddableRepository.Add(sut);
+            var newMember = RandomObjectFactory.GenerateRandomObject<TMember>();
+            AddableRepository.Add(newMember);
+            
+            // Test
+            var script = string.Format("Add-{0} -GroupName {1} -MemberName {2};", noun, sut.Name, newMember.Name);
+            psTestRunner.ExecuteCommand(script);
+
+            // Validate
+            var updatedGroup = searchableRepository.GetSingle<TGroupDeserializer>(sut.Name, ConfigTypes.Candidate).Single();
+            Assert.IsTrue(updatedGroup.Members.Contains(newMember.Name));
+            Assert.AreNotEqual(sut.Members.Count, updatedGroup.Members.Count);
+            Assert.IsTrue((updatedGroup.Members.Count - sut.Members.Count) == 1);
+            
+            // Cleanup
+            DeletableRepository.Delete(sut.SchemaName, sut.Name);
+            foreach (var member in updatedGroup.Members)
+            {
+                DeletableRepository.Delete(newMember.SchemaName, member);
+            } 
+        }
+    }
+}
